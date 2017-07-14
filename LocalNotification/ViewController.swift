0fdff2db176eb69a325cb427bfa2edb269407b52
Satish Phogat <satishphogat@satishphogat.local>
2017-07-14 15:51:34 +0530

    first commit

--- LocalNotification/ViewController.swift
+++ LocalNotification/ViewController.swift
@@ -7,18 +7,69 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
+    var checkAccessGranted : Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: .alert, completionHandler: {(allow,error) in
+        self.checkAccessGranted = allow
+        })
     }
 
-    override func didReceiveMemoryWarning() {
+    
+    
+  override  func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func applyNotification(_ sender: Any) {
+        
+        if checkAccessGranted{
+            let notificationContent = UNMutableNotificationContent()
+            notificationContent.title = "name of notification "
+            notificationContent.subtitle = "subtitle"
+            notificationContent.body = "Notification Body"
+            notificationContent.categoryIdentifier = "message"
+            
+            let notificationTrigger = UNTimeIntervalNotificationTrigger(
+                timeInterval: 60.0,
+                repeats: true)
+            
+            //Set the request for the notification from the above
+            let notificationRequest = UNNotificationRequest(
+                identifier: "10.second.message",
+                content:notificationContent,
+                trigger: notificationTrigger
+            )
+            
+            //Add the notification to the currnet notification center
+            //        UNUserNotificationCenter.current().add(
+            //            request, withCompletionHandler: nil)
+            
+            
+            
+            UNUserNotificationCenter.current().add(notificationRequest) {(error) in
+                if let notificationError = error {
+                    print(notificationError)
+                }
+                
+            }
+            
+            
+            
+        }
+
+    }
+    
+    
+    
 
 
 }
